bdc4f999fe416849237c0892ceb14cd2b86becc4
eFranchesca <112275326+eFranchesca@users.noreply.github.com>
2023-05-26 20:30:25 -0400

    Update WeatherForecast.js
    added useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() =>{
+    setLoaded(false);
+    }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
